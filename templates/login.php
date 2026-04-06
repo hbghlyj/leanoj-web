b893761079c222ec2ec72e4810f33b27eac23dd0
hbghlyj <53823634+hbghlyj@users.noreply.github.com>
2026-04-06 03:45:26 -0500

    Remove unused login.php and register.php (using Discuz forum auth)

--- templates/login.php
+++
@@ -1,8 +0,0 @@
-<h2>Login</h2>
-<form method="POST" action="index.php?action=login">
-  <input type="text" name="username" placeholder="Username" required>
-  <br>
-  <input type="password" name="password" placeholder="Password" required>
-  <br>
-  <input type="submit" value="Login">
-</form>
